3f86cb2cdb0b03d72844ffeed1c110e39b1a289c
Cristyn Magnus <cmagnus@gmail.com>
2019-10-04 12:47:06 -0400

    Define and use pointOffset

--- codeFromClass/graphics/main.py
+++ codeFromClass/graphics/main.py
@@ -3,6 +3,11 @@
 
 from graphics import *
 
+def pointOffset(point, x, y):
+    newX = point.x + x
+    newY = point.y + y
+    return Point(newX, newY)
+
 
 def drawRectangle(window, point1, point2, color):
     rectangle = Rectangle(point1, point2)
@@ -16,6 +21,9 @@ def main():
     circle = Circle(Point(250,250), 100)
     circle.draw(win)
     
+    pointA = Point(10,20)
+    pointB = pointOffset(pointA, 50, 50)
+    drawRectangle(win, pointA, pointB, "yellow")
 
     drawRectangle(win, Point(0,0), Point(40,30), "red")
     drawRectangle(win, Point(40,27), Point(56,39), "blue")
