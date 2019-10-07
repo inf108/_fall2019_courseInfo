3473901ef232e73482ac9995d7b142d0937f944e
Cristyn Magnus <cmagnus@gmail.com>
2019-10-07 11:47:46 -0400

    Clear out old unused code

--- codeFromClass/graphics/main.py
+++ codeFromClass/graphics/main.py
@@ -2,32 +2,14 @@
 # in YOUR folder & your branch
 
 from graphics import *
-
-def pointOffset(point, x, y):
-    newX = point.x + x
-    newY = point.y + y
-    return Point(newX, newY)
-
-
-def drawRectangle(window, point1, point2, color):
-    rectangle = Rectangle(point1, point2)
-    rectangle.setFill(color)
-    rectangle.draw(window)
+from house import *
+from roof import *
 
 #define main function
 def main():
-    win = GraphWin("My Circle", 500, 500)
-    
-    circle = Circle(Point(250,250), 100)
-    circle.draw(win)
-    
-    pointA = Point(10,20)
-    pointB = pointOffset(pointA, 50, 50)
-    drawRectangle(win, pointA, pointB, "yellow")
+    win = GraphWin("My House", 500, 500)
 
-    drawRectangle(win, Point(0,0), Point(40,30), "red")
-    drawRectangle(win, Point(40,27), Point(56,39), "blue")
-    drawRectangle(win, Point(200,400), Point(266,412), "green")
+    
     
     win.getMouse() # Pause to view result
     win.close()    # Close window when done
