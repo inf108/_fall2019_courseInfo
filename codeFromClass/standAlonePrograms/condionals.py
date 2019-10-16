43e6770ccfc91e3105da41c98a69d9f79902a1a6
Cristyn Magnus <cmagnus@gmail.com>
2019-10-16 11:58:58 -0400

    Add program that sandboxes with conditionals

---
+++ codeFromClass/standAlonePrograms/condionals.py
@@ -0,0 +1,29 @@
+
+a = "a"
+b = "b"
+
+print("a == b", a==b)
+
+print("a == a", a==a)
+
+print("A, B, or C")
+menu = input("What would you like to do?")
+
+if (menu == "a") or (menu == "A"):
+  print("A")
+elif (menu == "b") or (menu == "B"):
+  print("B")
+elif (menu == "c") or (menu == "C"):
+  print("C")
+else:
+  print("You didn't pick a valid option.")
+
+a = False
+b = False
+
+if (a and b):
+  print("A and B are both:", a)
+else:
+  print("asdfhiusdfgu;dafsh;io")
+
+
