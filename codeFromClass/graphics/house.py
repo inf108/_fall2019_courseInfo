925c06ba5002e6d652867e163c2ac73032df6743
Cristyn Magnus <cmagnus@gmail.com>
2019-10-21 11:54:34 -0400

    Add bricks to house

--- codeFromClass/graphics/house.py
+++ codeFromClass/graphics/house.py
@@ -14,12 +14,29 @@ def drawRectangle(window, point1, point2, color):
     rectangle.setFill(color)
     rectangle.draw(window)
 
+
+def rowOfBricks(window, x, y, color, heightOfBrick):
+    widthOfBrick = 20   
+    while (x < 350):
+        topLeftCorner = Point(x,y)
+        bottomRightCorner = Point(x+widthOfBrick, y + heightOfBrick)
+        drawRectangle(window, topLeftCorner, bottomRightCorner, color)
+        x = x + widthOfBrick
+
+
 def drawHouse(window, color):
     drawRectangle(window, Point(150,150), Point(350,350), color)
+    heightOfBrick = 10
+    x = 150
+    y = 150
+    while (y < 350):        
+        rowOfBricks(window, x, y, color, heightOfBrick)
+        y = y + heightOfBrick
+
 
 '''
 # for testing
 win = GraphWin("My Circle", 500, 500)
-drawHouse(win)
+drawHouse(win, "yellow")
 '''
 
