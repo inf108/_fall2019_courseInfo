3aa47dbb5fd898965f76f55e745a9b79469edb31
Cristyn Magnus <cmagnus@gmail.com>
2019-10-28 12:17:59 -0400

    Add modulo 26 to loop alphabet

--- codeFromClass/text/main.py
+++ codeFromClass/text/main.py
@@ -1,8 +1,12 @@
+rotation = 13
 textFile = open("test.txt", "r")
+
 for lines in textFile:
   lowerCaseLines = lines.lower()
   for letter in lowerCaseLines:
-    
-    print(ord(letter)-97, end = '')
+    numericLetter = ord(letter) - 97
+    numericLetter = numericLetter + rotation
+    numericLetter = numericLetter%26
+    print(numericLetter, end = '')
 textFile.close()
 
