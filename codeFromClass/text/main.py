343d0b17ccf8e29f7da79adc34df6def02d7109d
Cristyn Magnus <cmagnus@gmail.com>
2019-10-28 12:09:53 -0400

    Turn letters into ascii numbers

--- codeFromClass/text/main.py
+++ codeFromClass/text/main.py
@@ -2,6 +2,7 @@ textFile = open("test.txt", "r")
 for lines in textFile:
   lowerCaseLines = lines.lower()
   for letter in lowerCaseLines:
-    print(letter, end = '')
+    
+    print(ord(letter), end = '')
 textFile.close()
 
