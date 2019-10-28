af990f0d42967f2330dba444f53544fa6a4b51da
Cristyn Magnus <cmagnus@gmail.com>
2019-10-28 12:06:27 -0400

    No new lines after prints https://www.afternerd.com/blog/how-to-print-without-a-newline-in-python/

--- codeFromClass/text/main.py
+++ codeFromClass/text/main.py
@@ -2,6 +2,6 @@ textFile = open("test.txt", "r")
 for lines in textFile:
   lowerCaseLines = lines.lower()
   for letter in lowerCaseLines:
-    print(letter)
+    print(letter, end = '')
 textFile.close()
 
