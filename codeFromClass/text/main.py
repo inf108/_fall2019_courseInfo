c170db95fd3952484dc6637ec9d620ed0b087001
Cristyn Magnus <cmagnus@gmail.com>
2019-10-28 12:24:17 -0400

    Try to reencode as letter; breaks on space Tries to use chr to reincode the letter after doing the rot 13 math. It breaks at the space. We will fix this in the next class, probably by following the original plan of just filtering out things that aren't letters.

--- codeFromClass/text/main.py
+++ codeFromClass/text/main.py
@@ -5,8 +5,12 @@ for lines in textFile:
   lowerCaseLines = lines.lower()
   for letter in lowerCaseLines:
     numericLetter = ord(letter) - 97
+    print(numericLetter)
     numericLetter = numericLetter + rotation
+    print(numericLetter)
     numericLetter = numericLetter%26
-    print(numericLetter, end = '')
+    print(numericLetter)
+    encodedLetter = chr(numericLetter)
+    print(encodedLetter, end = '')
 textFile.close()
 
