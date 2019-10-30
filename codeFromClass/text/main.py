eeb91552708f080194e56bfabea08014fdcaadb0
Cristyn Magnus <cmagnus@gmail.com>
2019-10-30 11:52:55 -0400

    Verified that code works if text has no spaces & refactored for readability

--- codeFromClass/text/main.py
+++ codeFromClass/text/main.py
@@ -1,16 +1,19 @@
 rotation = 13
 textFile = open("test.txt", "r")
 
+def rotateLetter(letter):
+  numericLetter = ord(letter) - 97
+  numericLetter += rotation
+  numericLetter = numericLetter%26
+  numericLetter += 97
+  encodedLetter = chr(numericLetter)
+  return encodedLetter
+
 for lines in textFile:
   lowerCaseLines = lines.lower()
   for letter in lowerCaseLines:
-    numericLetter = ord(letter) - 97
-    print(numericLetter)
-    numericLetter = numericLetter + rotation
-    print(numericLetter)
-    numericLetter = numericLetter%26
-    print(numericLetter)
-    encodedLetter = chr(numericLetter)
+    encodedLetter = rotateLetter(letter)
     print(encodedLetter, end = '')
 textFile.close()
 
+print()
