be335f4c2aac3d036a89a2ffe329d588f5148ef0
Cristyn Magnus <cmagnus@gmail.com>
2019-11-06 12:23:55 -0500

    Finish processing fire data & visualize location & area

--- codeFromClass/visualization/visualize.py
+++ codeFromClass/visualization/visualize.py
@@ -1,8 +1,53 @@
+from graphics import *
+import math
+
 filename = "forestfires.csv"
+xyScale = 100
+columnsToUse = ['X', 'Y', 'month', 'day', 'temp', 'RH', 'wind', 'rain', 'area']
+radiusMultiplier = 2
+
+def cleanString(myString):
+  newString = ""
+  for letter in myString:
+    if (letter != "\n"):
+      newString += letter
+  return newString
+
+def cleanList(myList):
+  for i in range(len(myList)):
+    myList[i] = cleanString(myList[i])
+  return myList
+    
+
 forestFireData = open(filename, 'r')
 
+columnHeadings = forestFireData.readline().split(',')
+columnHeadings = cleanList(columnHeadings)
+
+rowsOfFireData = []
 
 for line in forestFireData:
-  print(line.split(','))
+  currentRow = {}
+  currentRowAsList = line.split(',')
+  currentRowAsList = cleanList(currentRowAsList)
 
-print(forestFireData.readline())
+  for i in range(len(currentRowAsList)): 
+    currentRow[columnHeadings[i]] = currentRowAsList[i]
+  #print(currentRow)
+  rowsOfFireData.append(currentRow)
+
+win = GraphWin("Forest Fires", 900, 900)
+
+for fire in rowsOfFireData:
+  x = float(fire["X"]) * xyScale - xyScale/2
+  y = float(fire["Y"]) * xyScale - xyScale/2
+  radius = math.sqrt(float(fire["area"])/math.pi) * radiusMultiplier
+  drawnFire = Circle(Point(x, y), radius)
+  #drawnFire.setFill("grey")
+  #drawnFire.setOutline("grey")
+  drawnFire.draw(win)
+  
+  
+win.getMouse() # Pause to view result
+win.close()    # Close window when done
+
