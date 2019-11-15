9ca452ff6f65304c608d3bf25556a8a7b351dbb9
Cristyn Magnus <cmagnus@gmail.com>
2019-11-15 11:51:21 -0500

    Add rectangle to drawing

--- codeFromClass/finalProject/main.py
+++ codeFromClass/finalProject/main.py
@@ -1,9 +1,16 @@
 from graphics import *
 
 def main():
-    win = GraphWin("Dalek", 100, 100)
+    win = GraphWin("Dalek", 300, 500)
+    
     c = Circle(Point(50,50), 10)
     c.draw(win)
+
+    r = Rectangle(Point(25, 25), Point(75, 75))
+    r.draw(win)
+
+
+    
     win.getMouse() # Pause to view result
     win.close()    # Close window when done
 
