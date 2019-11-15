422b3a1d752626ff595bf29233820f0b536be579
Cristyn Magnus <cmagnus@gmail.com>
2019-11-15 11:55:51 -0500

    Use x,y coordinates for center of circle instead of hardcoded values

--- codeFromClass/finalProject/main.py
+++ codeFromClass/finalProject/main.py
@@ -1,17 +1,20 @@
 from graphics import *
 
 def main():
-    win = GraphWin("Dalek", 300, 500)
-    
-    c = Circle(Point(50,50), 10)
-    c.draw(win)
+  win = GraphWin("Dalek", 300, 500)
 
-    r = Rectangle(Point(25, 25), Point(75, 75))
-    r.draw(win)
+  x = 150
+  y = 150
+  
+  c = Circle(Point(x,y), 10)
+  c.draw(win)
+
+  r = Rectangle(Point(25, 25), Point(75, 75))
+  r.draw(win)
 
 
-    
-    win.getMouse() # Pause to view result
-    win.close()    # Close window when done
+  
+  win.getMouse() # Pause to view result
+  win.close()    # Close window when done
 
 main()
