cac1cc6f8ed252049f2b3201a20174369ae70237
Cristyn Magnus <cmagnus@gmail.com>
2019-11-18 12:01:51 -0500

    Add function to draw dalek bottom

--- codeFromClass/finalProject/main.py
+++ codeFromClass/finalProject/main.py
@@ -1,9 +1,7 @@
 from graphics import *
 
 #(win, 75)
-def dalekTop(win, size):
-  x = 150
-  y = 150
+def dalekTop(win, x, y, size):
   radius = size
   domeColor = "grey"
   bodyColor = "grey"
@@ -15,22 +13,34 @@ def dalekTop(win, size):
   topLeftX = x - radius
   topLeftY = y
   bottomRightX = x + radius
-  bottomRightY = y + 2*radius
+  bottomRightY = y + radius
   
   r = Rectangle(Point(topLeftX, topLeftY), Point(bottomRightX, bottomRightY))
   r.setFill(bodyColor)
   r.draw(win)
 
+def dalekBottom(win, x, y, size):
+  top = y
+  bottom = y + size*3
+  topL = x - size
+  topR = x + size
+  bottomL = x - 2 * size
+  bottomR = x + 1.55 * size
+
+  p = Polygon(Point(topL, top), Point(topR, top), Point(bottomR, bottom), Point(bottomL, bottom))
+  p.setFill("grey")
+  p.draw(win)
+
 
 def main():
-  win = GraphWin("Dalek", 300, 500)
-
-  dalekTop(win, 75)
-
+  win = GraphWin("Dalek", 500, 1000)
   
+  dalekX = 250
+  dalekY = 150
+  size = 75
+  dalekTop(win, dalekX, dalekY, size)
 
-
-  
+  dalekBottom(win, dalekX, dalekY + size, size)
   win.getMouse() # Pause to view result
   win.close()    # Close window when done
 
