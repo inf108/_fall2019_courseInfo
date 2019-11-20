7d554015d4b1eab574d01a56cc5b0b73e95cf647
Cristyn Magnus <cmagnus@gmail.com>
2019-11-20 12:12:10 -0500

    Refactor and animate dalek

--- codeFromClass/finalProject/main.py
+++ codeFromClass/finalProject/main.py
@@ -19,28 +19,49 @@ def dalekTop(win, x, y, size):
   r.setFill(bodyColor)
   r.draw(win)
 
+  return [c, r]
+
 def dalekBottom(win, x, y, size):
   top = y
   bottom = y + size*3
   topL = x - size
   topR = x + size
-  bottomL = x - 2 * size
-  bottomR = x + 1.55 * size
+  bottomL = x - 1.55 * size
+  bottomR = x + 2 * size
 
   p = Polygon(Point(topL, top), Point(topR, top), Point(bottomR, bottom), Point(bottomL, bottom))
   p.setFill("grey")
   p.draw(win)
 
+  return [p]
+
+def createDalek(win, x, y, size):
+  top = dalekTop(win, x, y, size)
+  bottom = dalekBottom(win, x, y + size, size)
+
+  return top + bottom
+
 
 def main():
-  win = GraphWin("Dalek", 500, 1000)
-  
-  dalekX = 250
-  dalekY = 150
   size = 75
-  dalekTop(win, dalekX, dalekY, size)
+  dalekSpeed = 10
+  dalekX = 250
+  dalekY = 500
 
-  dalekBottom(win, dalekX, dalekY + size, size)
+  win = GraphWin("Dalek", 1440, 900)
+
+  dalek = createDalek(win, dalekX, dalekY, size)
+
+  while (True):
+    
+    #print(dalekX)
+    
+    for part in dalek:
+      part.move(dalekSpeed, 0)
+
+    time.sleep(0.1)
+
+  
   win.getMouse() # Pause to view result
   win.close()    # Close window when done
 
