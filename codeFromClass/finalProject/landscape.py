e29f2dbfc8a7970ad7a5991b9052fc5935e30e2a
Cristyn Magnus <cmagnus@gmail.com>
2019-12-04 11:19:48 -0500

    Create ground

--- codeFromClass/finalProject/landscape.py
+++ codeFromClass/finalProject/landscape.py
@@ -8,3 +8,12 @@ def randomColor():
   return (color_rgb(rand255(), rand255(), rand255()))
 
 
+def createGround(win, x, y, height):
+  groundColor = randomColor()
+  ground = Rectangle(Point(0, y*height), Point(x, y))
+  ground.setFill(groundColor)
+  ground.setOutline(groundColor)
+  ground.draw(win)
+
+  return [ground]
+
